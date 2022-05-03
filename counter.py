4f901c58e57022e68716ec3d8dc409e7fdccc145
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-03 09:59:03 +0530

    hist-commit

--- counter.py
+++ counter.py
@@ -1,10 +1,11 @@
 import http.server
 import random
-from prometheus_client import start_http_server, Counter, Gauge
+from prometheus_client import start_http_server, Counter, Gauge, Histogram
 import time
 
 no_of_requests=Counter('app_requests_count',' total no of requests',['app_name','endpoint'])
 total_requests_now=Gauge('app_requests_now','number of requests currently processed')
+items_served= Histogram('items_served_in_one_request','number of items served in one request', buckets= [0,10,20,30,40,50,60,70,100])
 
 PORT = 8000
 METRICS_PORT =8001
@@ -12,13 +13,15 @@ METRICS_PORT =8001
 class HandleRequests(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         no_of_requests.labels('rest_api',self.path).inc()
-        total_requests_now.inc()
-        time.sleep(5)
+        #total_requests_now.inc()
+        random_number=random.random()*100
+        items_served.observe(random_number)
+        #time.sleep(5)
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(bytes('{"message":"Hello World"}',"utf-8"))
-        total_requests_now.dec()
+        self.wfile.write(bytes('{"message":"Hello World "'+str(random_number)+"}","utf-8"))
+        #total_requests_now.dec()
         self.wfile.close()
 
 if __name__=="__main__":
