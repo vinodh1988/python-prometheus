b776d66ba30c71d44c9190242cb2b35ad167507b
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-03 10:14:34 +0530

    summary

--- counter.py
+++ counter.py
@@ -1,12 +1,12 @@
 import http.server
 import random
-from prometheus_client import start_http_server, Counter, Gauge, Histogram
+from prometheus_client import Summary, start_http_server, Counter, Gauge, Histogram
 import time
 
 no_of_requests=Counter('app_requests_count',' total no of requests',['app_name','endpoint'])
 total_requests_now=Gauge('app_requests_now','number of requests currently processed')
 items_served= Histogram('items_served_in_one_request','number of items served in one request', buckets= [0,10,20,30,40,50,60,70,100])
-
+items_served_summary =Summary('items_served_summary',"summary of items served")
 PORT = 8000
 METRICS_PORT =8001
 
@@ -16,6 +16,7 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
         #total_requests_now.inc()
         random_number=random.random()*100
         items_served.observe(random_number)
+        items_served_summary.observe(random_number)
         #time.sleep(5)
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
